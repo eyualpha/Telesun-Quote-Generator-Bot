cb708c4b1e285597911aaf02db549ff832388e79
Eyu Ashenafi <143643131+eyualpha@users.noreply.github.com>
2024-04-18 09:47:06 +0300

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,6 +1,6 @@
 function SettingWebHook() {
 
-  const botToken = "6786892817:AAHCVE_tl-2ffsUp0Eh0Gm_ZA3N8s9ujqkE";
-  const webhookUrl = "https://script.google.com/macros/s/AKfycbzN9lBeVVUpJYiaMPB7ZQ0FuReQcMw2aVxfp22-zQtJgR8k_glieHb3dwfh7Q16u2808w/exec";
+  const botToken = "add your bot token";
+  const webhookUrl = "your given webhook from google appScript";
   Bot.setWebHook(botToken, { url: webhookUrl });
 }
